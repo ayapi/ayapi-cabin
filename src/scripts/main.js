98fbaae0d89ec8f618d41411060a146d10ed35c1
upgrade_ayp <colors.aya@gmail.com>
2013-11-19 16:00:21 +0900

    fix #14 横に並べる系の要素のcssを調節する

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $.get('/side.html', function(data){
-    $('.wrapper').append(data);
+    $('.wrapper').append(data).css('minHeight',$('.side').height()+80);
   });
 });
